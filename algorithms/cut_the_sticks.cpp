2acf756a83001207b856bded0e4596c2f45e3268
bivil <bivilj@gmail.com>
2016-08-27 15:13:41 +0530

    cut the sticks ang git ignore

---
+++ algorithms/cut_the_sticks.cpp
@@ -0,0 +1,35 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    int N,i,item;
+    // N = 6 Items = 5 4 4 2 2 8 op = 6 4 2 1
+    cin >> N;
+    vector<int> stickL;
+    for(i=0;i<N;i++){
+        cin>>item;
+        stickL.push_back(item);
+    }
+    sort(stickL.begin(),stickL.end());
+    int a =stickL[0];
+    cout<<stickL.size()<<endl;
+    for(i=0;i<N;i++){
+     	if(stickL[i]==a)
+     	{
+     		stickL.erase(stickL.begin());i--;N--;
+     	}
+     	else{
+     		a=stickL[i];i--;
+     		cout<<stickL.size()<<endl;
+     	}
+
+    }
+
+    return 0;
+}
